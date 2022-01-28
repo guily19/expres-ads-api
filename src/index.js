545dd1f5f250fb7bb96c83076de61a1c9fca3ab5
Guillem Casanova <guillem.casanova@boats.com>
2022-01-28 19:48:19 +0100

    main: adding auth0 lib and authenticating endpoind

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { auth } = require('express-openid-connect');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const {startDatabase} = require('./database/mongo');
 const {insertAd, getAds, deleteAd, updateAd} = require('./database/ads');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
 
 // defining the Express app
 const app = express();
@@ -23,6 +26,18 @@ app.use(cors());
 app.use(morgan('combined'));
 
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3001',
+  clientID: 'j76RbbMD2XDogXACgAv3ZMUoEwba0zbC',
+  issuerBaseURL: 'https://dev-eo3ov8rm.us.auth0.com'
+};
+
+app.use(auth(config));
+
+
 // ... leave the app definition and the middleware config untouched ...
 
 // replace the endpoint responsible for the GET requests
@@ -33,21 +48,33 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
   const newAd = req.body;
-  await insertAd(newAd);
-  res.send({ message: 'New ad inserted.' });
+  if(req.oidc.isAuthenticated()){
+    await insertAd(newAd);
+    res.send({ message: 'New ad inserted.' });
+  } else {
+    res.send({ message: 'You dont have permission' });
+  }
 });
 
 app.delete('/:id', async (req, res) => {
-  await deleteAd(req.params.id);
-  res.send({ message: 'Ad removed.' });
+  if(req.oidc.isAuthenticated()){
+    await deleteAd(req.params.id);
+    res.send({ message: 'Ad removed.' });
+  } else {
+    res.send({ message: 'You dont have permission' });
+  }
 });
 
 // endpoint to update an ad
 app.put('/:id', async (req, res) => {
-  const updatedAd = req.body;
-  console.log('ID ->', req.params.id);
-  await updateAd(req.params.id, updatedAd);
-  res.send({ message: 'Ad updated.' });
+  if(req.oidc.isAuthenticated()){
+    const updatedAd = req.body;
+    console.log('ID ->', req.params.id);
+    await updateAd(req.params.id, updatedAd);
+    res.send({ message: 'Ad updated.' });
+  } else {
+    res.send({ message: 'You dont have permission' });
+  }
 });
 
 // start the in-memory MongoDB instance
